fc909de15e35f740519871cf54379f8e5c2b3c6f
abdulhai-02 <71089520+Abdulhai-02@users.noreply.github.com>
2026-05-09 21:49:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ if not TELEGRAM_TOKEN:
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN, parse_mode="HTML")
 
-MY_ADMIN_ID = 5143360493  
+MY_ADMIN_ID = 5266659205  
 ADMIN_LOG_CHAT_ID = -1003264764082
 MOSCOW_TZ = timezone(timedelta(hours=3))
 
